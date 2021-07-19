8d4ac3a55d883b097bad733e3d35756a0e0da865
regi <63766962+reiyiyi@users.noreply.github.com>
2021-07-19 16:02:23 +0900

    st_data.pyにおいて関数追加

--- st_data.py
+++ st_data.py
@@ -1,7 +1,35 @@
-#effect_id.json
+import os
+import json
+import pandas as pd
 
-#rev_effect_id.json
+BASE_DIR = 'data'
+
+#effect_id.json
+def load_effect_id_data():
+    FILE_NAME = BASE_DIR + '/effect_id.json'
+    if not os.path.exists(FILE_NAME):
+        return {}
+    with open(FILE_NAME, encoding='utf-8') as f:
+        return json.load(f)
+
+#rev_effect_id.csv
+def load_rev_effect_id_data():
+    FILE_NAME = BASE_DIR + '/rev_effect_id.csv'
+    if not os.path.exists(FILE_NAME):
+        return []
+    return pd.read_csv(FILE_NAME)
 
 #EXscore.csv
+def load_exscore_data():
+    FILE_NAME = BASE_DIR + '/EXscore.csv'
+    if not os.path.exists(FILE_NAME):
+        return []
+    return pd.read_csv(FILE_NAME)
 
 #user_data.json
+def load_user_data():
+    FILE_NAME = BASE_DIR + '/user_data.json'
+    if not os.path.exists(FILE_NAME):
+        return {}
+    with open(FILE_NAME, encoding='utf-8') as f:
+        return json.load(f)
