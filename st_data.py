b23584f76bccbb3571d813d96e73892d4bd40172
regi <63766962+reiyiyi@users.noreply.github.com>
2021-07-19 16:33:02 +0900

    st_data.pyに関数追加

--- st_data.py
+++ st_data.py
@@ -26,6 +26,10 @@ def load_exscore_data():
         return []
     return pd.read_csv(FILE_NAME)
 
+def save_exscore_data(save_data):
+    FILE_NAME = BASE_DIR + '/EXscore.csv'
+    save_data.to_csv(FILE_NAME, index=False)
+
 #user_data.json
 def load_user_data():
     FILE_NAME = BASE_DIR + '/user_data.json'
@@ -33,3 +37,8 @@ def load_user_data():
         return {}
     with open(FILE_NAME, encoding='utf-8') as f:
         return json.load(f)
+
+def save_user_data(save_data):
+    FILE_NAME = BASE_DIR + '/user_data.json'
+    with open(FILE_NAME, 'w', encoding='utf-8') as f:
+        json.dump(save_data, f, ensure_ascii=False, indent=4)
