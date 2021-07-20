605993726c1ea828039dbff885abe03663c019ec
regi <63766962+reiyiyi@users.noreply.github.com>
2021-07-20 12:52:50 +0900

    app.py編集

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 import st_login
 import st_data
 
@@ -9,32 +9,59 @@ def index():
 
 @app.route('/login')
 def login():
-    return "Hello."
+    return render_template('login.html')
 
-@app.route('/try_login')
+@app.route('/login/try')
 def try_login():
-    return "Hello."
+    #ユーザデータ
+    return render_template('form.html')
+
+@app.route('/login/signup')
+def signup():
+    #ユーザデータ
+    return render_template('form.html')
 
 @app.route('/logout')
 def logout():
-    return "Hello."
+    return render_template('logout.html')
 
 @app.route('/resister')
 def resister():
-    return "Hello."
+    #スコア登録した後に以下のデータを読み込み...
+    #EXスコアデータ
+    #譜面IDデータ
+    #逆参照譜面IDデータ
+    #ユーザデータ
+    return render_template('resister.html')
 
 @app.route('/notice')
 def notice():
-    return "Hello."
+    #ユーザデータ
+    return render_template('notice.html')
 
 @app.route('/rival')
 def rival():
-    return "Hello."
+    #ユーザデータ
+    #ユーザIDを乱数にするべき(4桁-4桁)
+    return render_template('rival.html')
 #改良する必要がある
 
-@app.route('/setting')
-def setting():
-    return "Hello."
+@app.route('/rival/follow')
+def rival_follow():
+    return render_template('rival-list.html')
+
+@app.route('/rival/follower')
+def rival_follower():
+    return render_template('rival-list.html')
+
+@app.route('/rival/search')
+def rival_search():
+    return render_template('rival-search.html')
+
+@app.route('/settings')
+def settings():
+    #ユーザデータ
+    return render_template('settings.html')
 
 if __name__ == "__main__":
    app.run(debug=True, host='0.0.0.0')
