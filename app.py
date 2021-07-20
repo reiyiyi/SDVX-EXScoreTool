8b210c3838a03a81c4b3cd7ed08c91375ce38568
regi <63766962+reiyiyi@users.noreply.github.com>
2021-07-20 11:16:21 +0900

    app.pyの

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 from flask import Flask, render_template
+import st_login
+import st_data
 
 app = Flask(__name__)
 @app.route('/')
 def index():
+    return "Hello."
 
+@app.route('/login')
+def login():
+    return "Hello."
+
+@app.route('/try_login')
+def try_login():
+    return "Hello."
+
+@app.route('/logout')
+def logout():
+    return "Hello."
+
+@app.route('/resister')
+def resister():
+    return "Hello."
+
+@app.route('/notice')
+def notice():
+    return "Hello."
+
+@app.route('/rival')
+def rival():
+    return "Hello."
+#改良する必要がある
+
+@app.route('/scoresetting')
+def scoresetting():
     return "Hello."
 
 if __name__ == "__main__":
