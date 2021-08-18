404a7d479b84fbd4750671971c50da79ad268f09
regi <63766962+reiyiyi@users.noreply.github.com>
2021-08-19 01:12:57 +0900

    rival修正

--- rival_resister.py
+++ rival_resister.py
@@ -1,5 +1,6 @@
 import st_data
 import st_login
+import copy
 
 def is_exist(rival):
     user_data = st_data.load_user_data()
@@ -12,26 +13,41 @@ def follow_rival():
     user = st_login.get_user_id()
     rival = user_data[user]["search-rival"]
     user_data[user]["search-rival"] = ""
-    user_data[user]["rival"][rival] = user_data[rival]["user-name"]
-    user_data[rival]["rev-rival"][user] = user_data[user]["user-name"]
+    user_data[user]["rival"][rival] = False
+    user_data[rival]["rev-rival"][user] = False
+    if rival in user_data[user]["rev-rival"]:
+        user_data[user]["rival"][rival] = True
+        user_data[rival]["rev-rival"][user] = True
+        user_data[user]["rev-rival"][rival] = True
+        user_data[rival]["rival"][user] = True
     st_data.save_user_data(user_data)
 
-def remove_rival(rival):
+def remove_rival():
     user_data = st_data.load_user_data()
     user = st_login.get_user_id()
+    rival = user_data[user]["search-rival"]
     user_data[user]["rival"].pop(rival)
     user_data[rival]["rev-rival"].pop(user)
+    if rival in user_data[user]["rev-rival"]:
+        user_data[user]["rev-rival"][rival] = False
+        user_data[rival]["rival"][user] = False
     st_data.save_user_data(user_data)
 
 def get_rival():
     user_data = st_data.load_user_data()
     user = st_login.get_user_id()
-    return user_data[user]["rival"]
+    return_data = user_data[user]["rival"].copy()
+    for rival in return_data:
+        return_data[rival] = user_data[rival]["user_name"]
+    return return_data
 
 def get_rev_rival():
     user_data = st_data.load_user_data()
     user = st_login.get_user_id()
-    return user_data[user]["rev-rival"]
+    return_data = user_data[user]["rev-rival"].copy()
+    for rival in return_data:
+        return_data[rival] = user_data[rival]["user_name"]
+    return return_data
 
 def get_rival_name(rival):
     user_data = st_data.load_user_data()
