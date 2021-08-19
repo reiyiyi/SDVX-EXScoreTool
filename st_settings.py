5f7851c876221b3a5bf5fe323b88f68274104cfa
regi <63766962+reiyiyi@users.noreply.github.com>
2021-08-19 18:12:52 +0900

    manual追記

--- st_settings.py
+++ st_settings.py
@@ -12,7 +12,7 @@ def score_settings_change(new_user_name, score_setting):
     if not id_check(new_user_name):
         return 'ユーザー名に使用することのできない文字が含まれています'
     if len(new_user_name) > 16 or len(new_user_name) < 1:
-        return 'ユーザーIDは1文字以上16文字以下にしてください'
+        return 'ユーザー名は1文字以上16文字以下にしてください'
     user = st_login.get_user_id()
     user_data = st_data.load_user_data()
     user_data[user]["user-name"] = new_user_name
